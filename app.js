5e6f95317619ffaf7988b4fdc1ac25ceddae2a52
zanti82 <zanti82@gmail.com>
2026-02-10 14:05:27 -0500

    feat: updating app.js con cartsRouter

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json()); // este me ayuda a convertir los datos de postman a objetos.
 app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
